e41e1be5ba2f34edcc8ae4f8860dbc9efa0e8268
Ka Lee Gary Wong <gary.wong@clearroute.io>
2025-03-18 11:47:41 +0000

    Adding path to file

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,19 +2,21 @@ import { test, expect } from '@playwright/test';
 import { authenticator } from 'otplib';
 
 test('has title', async ({ page }) => {
+  const username = process.env.TEST_USERNAME;
   const password = process.env.TEST_PASSWORD;
   const secret =  process.env.TEST_TOKEN;
-  const drive_url =  process.env.TEST_URL;
   const file =  process.env.TEST_URL;
-  await page.goto(drive_url);
+  await page.goto(file);
+  await page.getByRole('textbox').fill(username);
+  await page.getByRole('button', { name: 'Next' }).click();
   await page.getByRole('textbox', { name: 'Enter the password for gary.' }).fill(password);
   await page.getByRole('button', { name: 'Sign in' }).click();
   const token = authenticator.generate(secret);
   await page.getByLabel('code').fill(token);
   await page.getByRole('button', { name: 'Verify' }).click();
   await page.getByRole('button', { name: 'Yes' }).click();
-  await expect(page).toHaveTitle('SharePoint');
-  await page.goto(file);
+  await page.waitForLoadState('domcontentloaded');
+  await page.waitForTimeout(10000);
   await page.locator('iframe[name="WacFrame_Excel_0"]').contentFrame().getByRole('button', { name: 'File' }).click();
   await page.locator('iframe[name="WacFrame_Excel_0"]').contentFrame().getByText('Create a Copy').click();
   const downloadPromise = page.waitForEvent('download');
