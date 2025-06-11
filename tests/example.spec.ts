9288ec99da70c465510438270e429ebb5a8ed747
Gary Ka Lee Wong <garywong@duck.com>
2025-06-11 14:56:49 +0100

    With correct selector now

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -9,7 +9,7 @@ test('has title', async ({ page }) => {
   await page.goto(file);
   await page.getByRole('textbox').fill(username);
   await page.getByRole('button', { name: 'Next' }).click();
-  await page.getByRole('textbox', { name: 'Enter the password for gary.' }).fill(password);
+  await page.locator('input[type="password"]').fill(password);
   await page.getByRole('button', { name: 'Sign in' }).click();
   const token = authenticator.generate(secret);
   await page.getByLabel('code').fill(token);
@@ -24,7 +24,3 @@ test('has title', async ({ page }) => {
   const download = await downloadPromise;
   await download.saveAs('data/' + download.suggestedFilename());
 });
-
-//
-//https://clearroute24-my.sharepoint.com/:x:/g/personal/gary_wong_clearroute_io/EX3_Vs5eL-lOtlXgJfpZcooB1GVQeUhwhF3pxp0uVSZPKg?e=NYjOc3
-//https://clearroute24-my.sharepoint.com/personal/gary_wong_clearroute_io/_layouts/15/Doc.aspx?sourcedoc=%7BC1F4785E-D56D-4F04-9C83-01067D50AA6E%7D&file=Report%20Leave%20requests.xlsx&action=default&mobileredirect=true
